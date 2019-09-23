8ef0ccd22f438ff20ba07c6fcc76d4d6d4000368
Nicolas Ries <nries1@gmail.com>
2019-09-23 12:03:11 -0400

    added rendering for html

--- flask-app/app/routes.py
+++ flask-app/app/routes.py
@@ -1,6 +1,16 @@
+from flask import render_template
 from app import app
+
+# The two strange @app.route lines above the function are decorators, a unique
+# feature of the Python language. A decorator modifies the function that follows it.
+# A common pattern with decorators is to use them to register functions as callbacks
+# for certain events. In this case, the @app.route decorator creates an association
+# between the URL given as an argument and the function. In this example there are two
+# decorators, which associate the URLs / and /index to this function. This means that
+# when a web browser requests either of these two URLs, Flask is going to invoke this
+# function and pass the return value of it back to the browser as a response.
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+   return render_template('index.html')
---
+++ flask-app/app/templates/main.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', function() {
+    console.log('fetching posts');
+    fetch('/posts')
+    .then(res => res.json())
+    .then(data => {populatePosts(data)});
+  });
+
+  const populatePosts = posts => {
+    console.log(posts);
+      const container = document.getElementById('posts-container');
+      for (let i = posts.length - 1; i >= 0; i--) {
+        const postContainer = document.createElement('DIV');
+        postContainer.className = 'post-container';
+        postContainer.appendChild(contactInfo(posts[i]));
+        postContainer.appendChild(imgContainer(posts[i]));
+        container.appendChild(postContainer);
+      }
+      document.getElementById('spinner-container').style.display = 'none';
+  }
+
+const contactInfo = info => {
+    const container = document.createElement('DIV');
+    container.className = 'contact-info-container';
+    container.innerHTML = `<div><b>Name: </b>${info.name}</div><div><b>Email: </b>${info.email}</div>`
+    return container;
+}
+
+const imgContainer = info => {
+    const container = document.createElement('DIV');
+    container.className = 'user-image-container';
+    container.style.backgroundImage = `url(/user_images/${info.img})`;
+    container.style.backgroundPosition = info.img_position;
+    return container;
+}
